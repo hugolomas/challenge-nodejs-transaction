dab0b7d9609ef47c781068875c043e3db5492e57
hugolomas <hugoherrero@outlook.com>
2020-07-18 16:03:31 -0300

    transaction challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,57 @@ interface Balance {
   total: number;
 }
 
+interface CreateTrasactionDTO {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
     this.transactions = [];
+    
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const {income, outcome} = this.transactions.reduce((accumulator, transaction) => {
+      switch (transaction.type ){
+        case "income":
+          accumulator.income += transaction.value;
+          break;
+        case "outcome":
+          accumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+      return accumulator;
+    }, 
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    const total = income - outcome
+
+    return {income, outcome, total};
+    
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title,value,type}: CreateTrasactionDTO): Transaction {
+    const transaction = new Transaction({title,value,type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
